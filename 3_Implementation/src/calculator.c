c9c5396089e52f5a114fa8e799806fa18e834664
99003203 <75429050+99003203@users.noreply.github.com>
2020-12-05 23:07:16 +0530

    Update calculator.c

--- 3_Implementation/src/calculator.c
+++ 3_Implementation/src/calculator.c
@@ -1,1 +1,53 @@
+#include <calculator_operations.h>
+
+int add(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int subtract(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+int multiply(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int divide(int operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+    else
+        return operand1 / operand2;
+}
+int kmtom(int operand1, int operand 2)
+{
+  return operand1 * 1000;
+}
+int mtocm(int operand1, int operand2)
+{
+  return operand1 * 100;
+}
+int ftoi(int operand1, int operand2)
+{
+  return operand1 * 12;
+}
+int itocm(int operand1, int operand2)
+{
+  return operand1 * 2.54;
+}
+int cmtom(int operand1, int operand2)
+{
+  return operand1 * 0.01;
+}
+int ytom(int operand1, int operand2)
+{
+  return operand1 * 0.9144;
+}
+int ytocm(int operand1, int operand2)
+{
+  return operand1 * 91.44;
+}
 
