ea60fc8dd27493b2cf1d4e6dfe41b4741ab6bece
99003203 <75429050+99003203@users.noreply.github.com>
2020-12-06 20:22:41 +0530

    Update test_calculator.c

--- 3_Implementation/test/test_calculator.c
+++ 3_Implementation/test/test_calculator.c
@@ -6,10 +6,7 @@
 #define PROJECT_NAME    "calculator_operations.h"
 
 /* Prototypes for all the test functions */
-void test_add(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
+
 void test_kmtom(void);
 void test_mtocm(void);
 void test_ftoi(void);
@@ -30,10 +27,7 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_add);
-  RUN_TEST(test_subtract);
-  RUN_TEST(test_multiply);
-  RUN_TEST(test_divide);
+ 
   RUN_TEST(test_kmtom);
   RUN_TEST(test_mtocm);
   RUN_TEST(test_ftoi);
@@ -47,58 +41,54 @@ int main()
 }
 
 /* Write all the test functions */ 
-void test_add(void) {
-  TEST_ASSERT_EQUAL(30, add(10, 20));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1500, add(750, 7500));
-}
 
-void test_subtract(void) {
-  TEST_ASSERT_EQUAL(-3, subtract(0, 3));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1, subtract(1000, 900));
-}
-
-void test_multiply(void) {
-  TEST_ASSERT_EQUAL(0, multiply(1, 0));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(2, multiply(2, 5));
-}
-
-void test_divide(void) {
-  TEST_ASSERT_EQUAL(0, divide(1, 0));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(3, divide(2, 2));
-}
 
 void test_kmtom(void) {
   TEST_ASSERT_EQUAL(1000, kmtom(1));
+  
+    /* Dummy fail*/
+  TEST_ASSERT_EQUAL(1004, kmtom(2));
  }
  
 void test_mtocm(void) {
   TEST_ASSERT_EQUAL(200, mtocm(2));
+  
+  
+    /* Dummy fail*/
+  TEST_ASSERT_EQUAL(201, mtocm(1));
  }
  
 void test_ftoi(void) {
   TEST_ASSERT_EQUAL(36, ftoi(3));
+  
+   /* Dummy fail*/
+  TEST_ASSERT_EQUAL(37, ftoi(2));
 }
  
  void test_itocm(void) {
   TEST_ASSERT_EQUAL(10, itocm(4));
+   
+    /* Dummy fail*/
+  TEST_ASSERT_EQUAL(11, itocm(1));
 }
 
 void test_cmtom(void) {
   TEST_ASSERT_EQUAL(0, cmtom(5));
+  
+   /* Dummy fail*/
+  TEST_ASSERT_EQUAL(10, cmtom(3));
 }
 
 void test_ytom(void) {
   TEST_ASSERT_EQUAL(5, ytom(6));
+  
+   /* Dummy fail*/
+  TEST_ASSERT_EQUAL(5, ytom(1));
  }
  
  void test_ytocm(void) {
   TEST_ASSERT_EQUAL(640, ytocm(7));
+   
+    /* Dummy fail*/
+  TEST_ASSERT_EQUAL(641, ytocm(1));
  }
